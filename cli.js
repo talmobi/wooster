3c230af4258618ed89fa82dc11a7191529f18fa3
Christian Talmo <talmo.christian@gmail.com>
2017-09-17 16:05:25 +0300

    increased default cli timeout to 100ms ( was 15ms )

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ var timeout
 var argv = require('minimist')( process.argv.slice( 2 ) )
 var _timeout = argv.t || argv[ 'timeout' ]
 
-if ( _timeout == null ) _timeout = 15
+if ( _timeout == null ) _timeout = 100
 
 process.stdin.on('data', function ( chunk ) {
   buffer += chunk.toString( 'utf8' )
