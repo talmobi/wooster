f8e4905749d1cc2bf3dd760fc2d99fa288e3e288
Christian Talmo <talmo.christian@gmail.com>
2017-09-17 16:18:09 +0300

    refactored buffer clearing

--- cli.js
+++ cli.js
@@ -3,26 +3,38 @@
 var wooster = require('./snippet.js')
 var buffer = ''
 var timeout
+var bufferClearTimeout
 
 var argv = require('minimist')( process.argv.slice( 2 ) )
 var _timeout = argv.t || argv[ 'timeout' ]
 
 if ( _timeout == null ) _timeout = 100
 
+function clearConsole () {
+  process.stdout.write( '\x1bc' )
+}
+
 process.stdin.on('data', function ( chunk ) {
   buffer += chunk.toString( 'utf8' )
   if ( _timeout > 0 ) {
-    clearTimeout(timeout)
-    timeout = setTimeout(function () {
+    clearTimeout( timeout )
+    timeout = setTimeout( function () {
       run()
-    }, _timeout)
+    }, _timeout )
   } else {
     run()
   }
 
+  clearTimeout( bufferClearTimeout )
+
   function run () {
+    clearConsole()
     var output = wooster( buffer )
-    buffer = ''
+
+    bufferClearTimeout = setTimeout( function () {
+      buffer = ''
+    }, 1500 )
+
     process.stdout.write( output )
   }
 })
