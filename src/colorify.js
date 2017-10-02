0cb48f8283256cd6d4f8fd694b0e78eb75b436ad
Christian Talmo <talmo.christian@gmail.com>
2017-10-02 20:21:16 +0300

    fixed colorify

--- src/colorify.js
+++ src/colorify.js
@@ -1,7 +1,7 @@
 var clc = require( 'cli-color' )
 
 function colorify ( text, color ) {
-  if ( clc && color ) {
+  if ( clc && clc[ color ] ) {
     return clc[ color ]( text )
   } else {
     return text
