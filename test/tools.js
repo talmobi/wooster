f0f57e0cba7de73dd91ee9b88a3b8878e536a6e2
Christian Talmo <talmo.christian@gmail.com>
2017-10-06 20:10:16 +0300

    sped up the tests for reasonable machines..

--- test/tools.js
+++ test/tools.js
@@ -44,7 +44,7 @@ function createIOHandler ( callback ) {
       console.log( 'IOHandler timed out' )
       callback( _buffer )
     }
-  }, 1000 * 10 )
+  }, 1000 * 3 )
 
   return function handleIO ( chunk ) {
     if ( _done ) return undefined
@@ -63,7 +63,7 @@ function createIOHandler ( callback ) {
 
 function execWait ( file, cmd, args, callback ) {
   var startTime = Date.now()
-  var limit = 1000 * 15
+  var limit = 1000 * 3
 
   attempt()
 
