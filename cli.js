32349e95876e8457976ea5055f072e6fecd47da0
Christian Talmo <talmo.christian@gmail.com>
2017-11-21 20:21:18 +0200

    fix: keep running and listening on stdio indefinitely by default ( like in previous versions ). Added -b, --bail argument to exit after first parse with correct exit code if error was found or not, added -V, --version cli argument

--- cli.js
+++ cli.js
@@ -3,17 +3,29 @@
 var wooster = require( './dist/bundle.min.js' )
 var buffer = ''
 var timeout
-var exitTimeout
-var hasErrors = false
+var bufferClearTimeout
+var hasHadErrors = false
 
 var argv = require( 'minimist' )( process.argv.slice( 2 ), {
   alias: {
-    'debounce': [ 'timeout', 't', 'd' ]
+    'debounce': [ 'timeout', 't', 'd' ],
+    'bail': [ 'b', 'exit', 'e', 'x' ], // bail and exit after first parse
+    'version': [ 'V' ]
   }
 } )
+
+if ( argv[ 'version' ] ) {
+  var path = require( 'path' )
+  var pkg = require( path.join( __dirname, 'package.json' ) )
+  console.log( 'wooster version: ' + pkg.version )
+  process.exit( 0  )
+}
+
 var _timeout = argv[ 'debounce' ]
 
 if ( _timeout == null ) _timeout = 100
+
+var shouldBail = !!argv[ 'bail' ]
 
 function clearConsole () {
   process.stdout.write( '\x1bc' )
@@ -30,21 +42,27 @@ process.stdin.on( 'data', function ( chunk ) {
     run()
   }
 
-  clearTimeout( exitTimeout )
+  clearTimeout( bufferClearTimeout )
 
   function run () {
+    var hasErrors = false
     var output = wooster( buffer )
 
     if ( output !== buffer ) {
       hasErrors = true
+      hasHadErrors = true
     }
 
-    exitTimeout = setTimeout( function () {
-      if ( hasErrors ) {
-        process.exit( 1 )
-      } else {
-        process.exit( 0 )
+    bufferClearTimeout = setTimeout( function () {
+      if ( shouldBail ) {
+        if ( hasHadErrors ) {
+          process.exit( 1 ) // exit failure
+        } else {
+          process.exit( 0 ) // exit success
+        }
       }
+
+      buffer = ''
     }, 100 )
 
     if ( hasErrors ) {
