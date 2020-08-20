a54ea1e09a3629f96149a4ad14ed703354a2e318
Christian Talmo <talmo.christian@gmail.com>
2020-08-20 03:03:29 +0300

    debug: mac/win gha ci

--- test/test-rollup.js
+++ test/test-rollup.js
@@ -104,6 +104,10 @@ test( 'error rollup build', function ( t ) {
     sourcePath,
     targetPath,
     function ( data ) {
+      t.comment( '' )
+      t.comment( data )
+      t.comment( '' )
+
       t.notEqual(
         tools.normalize( data ).indexOf( 'error' ),
         -1,
